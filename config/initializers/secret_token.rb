42ceccbbe521b968cde858b6c17ceea085dc0983
qurt <q.rainheart@gmail.com>
2014-03-07 18:44:58 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QMoney::Application.config.secret_key_base = '0b9ce31c3ff9ee1e706603e983b8f39eebec316ad6b2700d0e1d2106bd1d494449607d82af755249a773d636346114920fc0912491bf2c8881b4b700a66d099f'
