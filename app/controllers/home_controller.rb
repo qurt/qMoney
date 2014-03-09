d814cbc60554cbb12f071154f0313f19e7250e99
Qurt <q.rainheart@gmail.com>
2014-03-10 03:20:47 +0400

    add filter for operations by accounts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
+    account = 0
+    if params.has_key?(:a)
+      account = params[:a].to_i
+    end
     @accounts = Account.all
+    @sum = 0
+    @accounts.each do |item|
+      @sum += item.value
+    end
+    if account == 0
+      @operations = Operation.all
+    else
+      @operations = Account.find(account).operations
+    end
   end
 end
