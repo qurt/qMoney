9c8dde7545ea3677ff3e29699e88234813c67e27
Qurt <q.rainheart@gmail.com>
2014-03-27 00:13:32 +0400

    add user info into nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
+      session[:user_name] = user.name
       redirect_to home_index_url
     else
       redirect_to login_url, alert: 'Неверная комбинация имени и пароля'
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:user_name] = nil
     redirect_to login_url
   end
 end
