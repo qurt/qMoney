2907a23d2b7c092d4195f7b387d8a2331ccc0e71
Qurt <q.rainheart@gmail.com>
2014-04-15 18:54:25 +0400

    add date filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,11 @@ class HomeController < ApplicationController
     if params.has_key?(:a)
       account = params[:a].to_i
     end
+    now = Time.now.midnigth
+    start_date = now - now.day
     @accounts = Account.order(:name)
     if account == 0
-      @operations = Operation.order(created_at: :desc)
+      @operations = Operation.where('created_at >= ?', start_date).order(created_at: :desc)
     else
       @operations = Account.find(account).operations
     end
