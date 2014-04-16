fc59df92db0d9acc7e66e7ce9b56899240d4d7a7
qurt <q.rainheart@gmail.com>
2014-04-16 19:34:50 +0400

    Вывод затраченных сумм, сгруппированных по категриям

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,9 +8,18 @@ class HomeController < ApplicationController
     start_date = Time.mktime(now.year, now.month)
     @accounts = Account.order(:name)
     if account == 0
-      @operations = Operation.where('created_at >= ?', start_date).order(created_at: :desc)
+      @operations = Operation.where('operations.created_at >= ?', start_date).order(created_at: :desc)
     else
       @operations = Account.find(account).operations
     end
+    cat = @operations.group(:category_id).sum(:value)
+    tmp2 = []
+    cat.each do |item|
+      if item[0] > 0
+        tmp = Category.find(item[0])
+        tmp2 << [tmp.title, item[1]]
+      end
+    end
+    @categories = tmp2
   end
 end
