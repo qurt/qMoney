0de01cba2780ac691cf5ccbddc32c7bc8ca70937
Qurt <q.rainheart@gmail.com>
2014-06-14 23:48:29 +0400

    Добавлены комментарии

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    # Проверяем параметры для фильтров. По умолчанию устанавливаем их в 0
     account = 0
     category = 0
     if params.has_key?(:a)
@@ -8,11 +9,11 @@ class HomeController < ApplicationController
     if params.has_key?(:c)
       category = params[:c].to_i
     end
-
+    # Get accounts list
     @accounts = Account.order(:name)
-
+    # Get operations list
     @operations = get_operations(account, category)
-
+    # Generate categories list from operations
     @categories = {}
     @operations.each do |item|
       if item.category_id != 0 and item.type == 0
@@ -23,6 +24,7 @@ class HomeController < ApplicationController
         end
       end
     end
+    # Get credits list
     @credits = Credit.where.not(value:0)
   end
 
