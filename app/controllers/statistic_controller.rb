3494c1c2bf1f61563bf09a69b79501500eacd279
Qurt <q.rainheart@gmail.com>
2014-07-25 22:13:11 +0400

    Подсчет средней величины трат за месяц в общем и по категориям

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -32,4 +32,85 @@ class StatisticController < ApplicationController
         :active_a   => active_a
     }
   end
+  def average_spending
+    operations = Operation
+      .select('SUM(value) as op_sum, category_id, type, date(operation_date) as op_date')
+      .group('op_date, category_id, type')
+      .where('type = 0')
+
+    # Считаем среднюю сумму трат в месяц за все время
+    sum = {}
+    operations.each do |item|
+      month = item.op_date.month.to_i
+      year = item.op_date.year.to_i
+      if sum[year].nil?
+        sum[year] = {}
+        sum[year][month] = 0
+      else
+        if sum[year][month].nil?
+          sum[year][month] = 0
+        end
+      end
+      sum[year][month] += item.op_sum.to_f
+    end
+    average = 0
+    month_count = 0
+    sum.each do |y, item|
+      month_count += item.size
+      item.each do |m, item_month|
+        average += item_month
+      end
+    end
+    average = average / month_count
+    # Считаем среднюю сумму трат в месяц за все время, разделенную по категориям
+    category_sum = {}
+    operations.each do |item|
+      month = item.op_date.month.to_i
+      year = item.op_date.year.to_i
+      category_id = item.category_id
+      if category_sum[category_id].nil?
+        category_sum[category_id] = {
+            :title => get_category_title(category_id),
+            :data => {}
+        }
+      end
+      if category_sum[category_id][:data][year].nil?
+        category_sum[category_id][:data][year] = {}
+      end
+      if category_sum[category_id][:data][year][month].nil?
+        category_sum[category_id][:data][year][month] = 0
+      end
+      category_sum[category_id][:data][year][month] += item.op_sum
+    end
+    category_average = {}
+    category_sum.each do |id, item|
+      category_average[id] = {
+        :title => category_sum[id][:title],
+        :average => 0
+      }
+      item[:data].each do |y, year_data|
+        year_data.each do |m, month_data|
+          category_average[id][:average] += month_data
+        end
+      end
+    end
+    category_average.each do |id, c_average|
+      c_average[:average] = c_average[:average] / month_count
+    end
+    # Формируем график, который будет содержать:
+    # - Сумму всех трат в месяц
+    # - Сумму трат по категориям в месяц
+    # Формирование результатат для отправки в шаблон
+    @result = {
+      :average => average,
+      :category_average => category_average
+    }
+  end
+
+  private
+  # Вспомогательные методы
+  def get_category_title(category_id)
+    category_item = Category.find(category_id)
+    title = category_item.title
+  end
 end
