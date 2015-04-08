d9700f563ecd7f5ab7ea7c68abe6baa3b78dbc6e
Qurt <q.rainheart@gmail.com>
2015-04-08 21:45:00 +0300

    Update

--- app/controllers/shop_list_controller.rb
+++ app/controllers/shop_list_controller.rb
@@ -1,10 +1,40 @@
 class ShopListController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+    @list_item  = ShopList.new(list_params)
+    respond_to do |format|
+      if @list_item.save
+        format.json { render json: @list_item, status: :created}
+      else
+        format.json { render json: @list_item.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
   def new
+
+  end
+
+  def edit
+
   end
 
   def update
   end
 
-  def delete
+  def destroy
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :active, :value)
   end
 end
