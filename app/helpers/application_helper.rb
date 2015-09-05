8c4020f5e2eddbcaa591696e224a364952426007
Andrey Orekhov <q.rainheart@gmail.com>
2015-09-05 17:50:34 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
     def category_options(categories)
-        result = []
+        result = {}
         parents = categories.where('parent_id = 0')
         parents.each do |item|
-            result << [item.title, item.id]
-            if categories.where(parent_id: item.parent_id).count() > 0
-                categories.where(parent_id: item.parent_id).each do |child|
-                    result << [item.title + '/' + child.title, child.id]
+            result[item.title] = []
+            if categories.where(parent_id: item.id).count() > 0
+                categories.where(parent_id: item.id).each do |child|
+                    result[item.title] << [item.title + '/' + child.title, child.id]
                 end
             end
         end
