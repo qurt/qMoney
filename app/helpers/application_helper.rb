0cb101a6967830d6d6c237c592f0b35c794f722d
Andrey Orekhov <q.rainheart@gmail.com>
2015-09-05 17:27:00 +0300

    no message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
-  def category_options(categories)
+    def category_options(categories)
+        result = []
+        parents = categories.where('parent_id = 0')
+        parents.each do |item|
+            result << [item.title, item.id]
+            if categories.where(parent_id: item.parent_id).count() > 0
+                categories.where(parent_id: item.parent_id).each do |child|
+                    result << [item.title + '/' + child.title, child.id]
+                end
+            end
+        end
 
-  end
+        result
+    end
 end
