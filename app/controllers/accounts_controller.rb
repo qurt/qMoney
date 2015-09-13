8bcf6fd83a6bf2a33fb9334a91f6f5e3550cda0c
Andrey Orekhov <q.rainheart@gmail.com>
2015-09-13 18:44:13 +0300

    parse attributes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -95,6 +95,6 @@ class AccountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.require(:account).permit(:name, :value)
+      params.require(:account).permit(:name, :value, moneybox_attributes: [:summary, :percentage])
     end
 end
