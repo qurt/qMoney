bbea8cbfd2228f4c7b7a13f8d69f8702a111482d
Andrey Orekhov <q.rainheart@gmail.com>
2015-11-11 00:04:23 +0300

    Проверяем не идет ли перемещение в этот же кошелек

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,8 +1,17 @@
-class Operation < ActiveRecord::Base
-  validates :value, presence: true, numericality: true
-  
-  belongs_to :account
-  belongs_to :category
+class TransferValidator < ActiveModel::Validator
+    def validate(record)
+        if record.account_id == record.transfer
+            record.errors[:base] << "Нельзя переместить в самого себя"
+        end
+    end
+end
 
-  self.inheritance_column = nil
+class Operation < ActiveRecord::Base
+    validates :value, presence: true, numericality: true
+    validates_with TransferValidator
+
+    belongs_to :account
+    belongs_to :category
+
+    self.inheritance_column = nil
 end
