8ec97259b2259163d51c50687391ca3af400b1f3
Andrey Orekhov <q.rainheart@gmail.com>
2015-12-27 23:58:42 +0300

    add collection select

--- app/helpers/select2_helper.rb
+++ app/helpers/select2_helper.rb
@@ -1,9 +1,9 @@
 module Select2Helper
     def select2(id, model, options_for_select, selected = nil, attributes = nil)
-        select_tag = '<select id="' + id + '" name="' + model + '"'
+        select_tag = "<select id=\"#{id}\" name=\"#{model}\""
         unless attributes.nil?
             attributes.each do |name, value|
-                select_tag += ' ' + name.to_s + '="' + value + '"'
+                select_tag += " #{name}=\"#{value}\""
             end
         end
         select_tag += '>'
@@ -11,17 +11,52 @@ module Select2Helper
         options = ''
         options_for_select.each do |key, value|
             if value == selected
-                options += '<option value="'+value.to_s+'" selected>' + key.to_s + '</options>'
+                options += "<option value=\"#{value}\" selected>#{key}</options>"
             else
-                options += '<option value="'+value.to_s+'">' + key.to_s + '</options>'
+                options += "<option value=\"#{value}\">#{key}</options>"
             end
         end
 
         select_tag += options
         select_tag += '</select>'
-        script = '<script>$("#' + id + '").select2();</script>'
+        script = "<script>$('##{id}').select2();</script>"
 
         result = select_tag + script
         return result.html_safe
+    end
+
+    def select2_collection(model, field, collection, value, title, selected = nil, attributes = nil)
+        id = "#{model}_#{field}"
+        name = "#{model}[#{field}]"
+
+        select_tag = "<select id=\"#{id}\" name=\"#{name}\""
+        unless attributes.nil?
+            attributes.each do |name, value|
+                select_tag += ' ' + name.to_s + '="' + value + '"'
+            end
+        end
+        select_tag += '>'
+
+        options  = ''
+        collection.each do |item|
+            option_value = item[value]
+            option_title = item[title]
+            if value == selected
+                options += "<option value=\"#{option_value}\" selected>#{option_title}</option>"
+            else
+                options += "<option value=\"#{option_value}\">#{option_title}</option>"
+            end
+        end
+
+        select_tag += options
+        select_tag += '</select>'
+        script = "<script>$('##{id}').select2();</script>"
+
+        result = select_tag + script
+        return result.html_safe
+    end
+
+    def select2_grouped
+
     end
 end
