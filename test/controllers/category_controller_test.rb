1daceaa4183440e1d8b87dee96d74c6561e6ae2d
Andrey Orekhov <q.rainheart@gmail.com>
2016-02-13 21:18:26 +0300

    update tests

--- test/controllers/category_controller_test.rb
+++ test/controllers/category_controller_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
 
 class CategoryControllerTest < ActionController::TestCase
-  test "should get add" do
-    get :add
-    assert_response :success
-  end
+  # test "should get add" do
+  #   get :add
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get :edit
+  #   assert_response :success
+  # end
 
-  test "should get delete" do
-    get :delete
-    assert_response :success
-  end
+  # test "should get delete" do
+  #   get :delete
+  #   assert_response :success
+  # end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get :show
+  #   assert_response :success
+  # end
 
 end
