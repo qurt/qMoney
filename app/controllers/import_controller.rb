5601bf37e0e8d0a162e08df1af1548cd384fe829
Andrey Orekhov <q.rainheart@gmail.com>
2016-04-07 20:49:10 +0300

    update

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -6,25 +6,39 @@ class ImportController < ApplicationController
         list = []
         file = params[:file]
         File.foreach(file.path) do |row|
-            item = CSV.parse(row.gsub('"', "'"))
-            logger.debug item.to_s
+            item = row.split(';')
+            item.each do |field|
+                field[0] = '' if field[0] == '"'
+                field[-1] = '' if field[-1] == '"'
+            end
+
+            operation = {}
+
+            # Дата импортируемой операции
+            operation[:date] = item[1].length > 0 ? item[1] : item[0]
+            # Определение кошелька
+            operation[:account] = findRule('account', item[2]) || nil
+            # Сумма
+            operation[:value] = item[6].to_f.abs
 
             list << {
-                date: item[1] || item[0],
+                date: item[1].length > 0 ? item[1] : item[0],
                 account: item[2],
-                value: item[6],
+                value: item[6].to_f,
                 category: item[8],
                 category_id: item[9],
-                tag: item[10]
+                tag: item[10],
+                operation: operation
             }
         end
-        logger.debug list.to_s
-        flash[:list]
-        redirect_to import_operations_list_path
+        @list = list
+        render "operations_list"
+        # flash[:list]
+        # redirect_to import_operations_list_path
     end
 
     def operations_list
-        if params[:list]
+        if flash[:list]
             @list = flash[:list]
             respond_to do |format|
               format.html
@@ -33,5 +47,16 @@ class ImportController < ApplicationController
         else
             redirect_to import_operations_path
         end
+    end
+
+    def create_from_list
+
+    end
+
+    private
+    def findRule(value)
+        rule = ImportRules.where('rule = ?', value)
+        return rule if rule
+        return false
     end
 end
---
+++ test/models/import_rule_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ImportRuleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
