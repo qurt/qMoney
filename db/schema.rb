dfd403afed6ce190d0d946e0d346e24adea17e13
Andrey Orekhov <q.rainheart@gmail.com>
2016-05-25 12:57:11 +0300

    small fix

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20160503184434) do
     t.datetime "updated_at"
   end
 
+  create_table "goods", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "category_id",             default: 0
+  end
+
   create_table "import_rules", force: :cascade do |t|
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
@@ -87,6 +94,13 @@ ActiveRecord::Schema.define(version: 20160503184434) do
     t.decimal  "expired_in"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shop_lists", force: :cascade do |t|
+    t.integer  "goods_id"
+    t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tags", force: :cascade do |t|
